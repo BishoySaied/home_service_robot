98e34680e1269ddf35f6bff27afc8eae0d5f2c8a
BishoySaied <bishoy.saber@gmail.com>
2020-05-31 10:00:37 +0000

    created add_markers node and modified pick_objects node to have 2 goals instead of one, created a launch scripts for each node

---
+++ scripts/add_marker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xterm  -e  " source develop/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm  -e  "  source develop/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
+sleep 5
+xterm  -e  " source develop/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm  -e  " source develop/setup.bash; rosrun add_markers add_markers" &
+sleep 5
